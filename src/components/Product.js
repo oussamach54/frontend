e4fbdca074fe482a006c4a80d59b63196f3c3d62
oussamach54 <102917157+oussamach54@users.noreply.github.com>
2025-11-07 23:59:46 +0100

    Update Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,31 +1,32 @@
-import { Card } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { resolveImageURL } from "../utils/media";
 
+function Product({ product = {} }) {
+  const id = product.id ?? product._id;
+  if (!id) return null;
 
-import React from 'react'
+  const img = resolveImageURL(product.image_url || product.image || product.thumbnail);
 
-function Product({ product }) {
-    return (
-        <div>
-            <Card className="mb-4 rounded">
+  return (
+    <Card className="mb-4 rounded">
+      <Link to={`/product/${id}`}>
+        <Card.Img variant="top" src={img} height="162" />
+      </Link>
 
-                <Card.Body>
-                <Link to={`/product/${product.id}`}>
-                    <Card.Img variant="top" src={product.image} height="162" />
-                </Link>
-                    <Link to={`/product/${product.id}`}>
-                        <Card.Title as="div">
-                            <strong>{product.name}</strong>
-                        </Card.Title>
-                    </Link>
+      <Card.Body>
+        <Link to={`/product/${id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Link>
 
-                    <Card.Text as="h3">
-                        MAD {product.price}
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-        </div>
-    )
+        <Card.Text as="h3">MAD {Number(product.price || 0)}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
 }
 
-export default Product
+export default Product;
+
